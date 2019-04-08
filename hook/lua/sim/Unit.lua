00bdf8ef3873e7fd311f218e39729eb2be8a1e37
Douglas Gaskell <1400380+douglasg14b@users.noreply.github.com>
2019-04-07 23:14:34 -0700

    Makes it so own units are not destroyed on first death.
    Instead they are healed. This prevents a loss of move or attack orders

--- hook/lua/sim/Unit.lua
+++ hook/lua/sim/Unit.lua
@@ -28,7 +28,70 @@ do
     local oUnit = Unit;
 
     Unit = Class(oUnit) {
-        IsZombie = false,
+		IsZombie = false,
+
+		DoTakeDamage = function(self, instigator, amount, vector, damageType)
+
+			
+			if not AreZombiesSetup then LOG("::Zombies:: Setting up Zombies");  SetupZombies() end
+			
+			local ownArmy = self:GetAIBrain().Name
+			--SPEW("::Zombies:: Army: " .. self:GetArmy())
+			--SPEW("::Zombies:: AI Brain: " .. self:GetAIBrain().Name)
+			local preAdjHealth = self:GetHealth()
+			if preAdjHealth - amount > 0 then 
+				oUnit.DoTakeDamage(self, instigator, amount, vector, damageType)
+				return
+			elseif self.IsZombie or not AreZombiesSetup then
+				oUnit.DoTakeDamage(self, instigator, amount, vector, damageType)
+				return
+			elseif ownArmy ~= ZombieArmy then
+				SPEW("::Zombies:: Unit Killed: Self: " .. self:GetArmy() .. " Instigator: " .. instigator:GetArmy())
+				oUnit.DoTakeDamage(self, instigator, amount, vector, damageType)
+				return
+			end
+
+			local overkillRatio = 0.0
+			local excess = preAdjHealth - amount
+			local maxHealth = self:GetMaxHealth()
+			SPEW("::Zombies:: Calculating Overkill. Max health: " .. maxHealth)
+			if excess < 0 and maxHealth > 0 then
+				overkillRatio = -excess / maxHealth
+			end
+
+			self.IsZombie = true
+
+			self:ForkThread( self.HandlePseudoDeath, self, instigator,  overkillRatio)
+
+			--local bp = self:GetBlueprint()
+			--SPEW("::Zombies:: Healing Unit. Max health: " .. bp.Defense.maxHealth)
+			--SPEW("::Zombies:: Healing Unit. Max health: " .. self:GetMaxHealth())
+			self:AdjustHealth(self, maxHealth)
+		end,
+
+		HandlePseudoDeath = function(self, instigator, overkillRatio)
+			local layer = self:GetCurrentLayer()
+			local bp = self:GetBlueprint()
+			if layer == 'Water' and bp.Physics.MotionType == 'RULEUMT_Hover' then
+				self:PlayUnitSound('HoverKilledOnWater')
+			elseif layer == 'Land' and bp.Physics.MotionType == 'RULEUMT_AmphibiousFloating' then
+				-- Handle ships that can walk on land
+				self:PlayUnitSound('AmphibiousFloatingKilledOnLand')
+			else
+				self:PlayUnitSound('Killed')
+			end
+
+			-- BOOM!
+			if self.PlayDestructionEffects then
+				self:CreateDestructionEffects(overkillRatio)
+			end
+
+			-- Flying bits of metal and whatnot. More bits for more overkill.
+			if self.ShowUnitDestructionDebris and overkillRatio then
+				self.CreateUnitDestructionDebris(self, true, true, overkillRatio > 2)
+			end
+		end,
+
 		CreateWreckage = function( self, overkillRatio )
 			if not AreZombiesSetup then LOG("::Zombies:: Setting up Zombies");  SetupZombies() end
 			if self.IsZombie or not AreZombiesSetup then
