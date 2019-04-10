a2cf23c7b8d3fa6e212d57f71134161bdd1fd4e3
Douglas Gaskell <1400380+douglasg14b@users.noreply.github.com>
2019-04-09 18:41:22 -0700

    Adds vampire options to lobby. Increases slot count for selection to 20.
    Needed more slots as some maps are 10 v 10

--- lua/AI/LobbyOptions/lobbyoptions.lua
+++ lua/AI/LobbyOptions/lobbyoptions.lua
@@ -1,13 +1,24 @@
 AIOpts = {
     {
         default = 1,
-        label = "<LOC zombies_0001>Zombie Army",
-        help = "<LOC zombies_0002>Set the army that will be the zombie",
+        label = "<LOC zombies_0001>Zombies: Player Slot",
+        help = "<LOC zombies_0002>The map slot that will be designated as the zombie",
         key = 'ZombieArmy',
         value_text = "%s",
-        value_help = "<LOC zombies_0003>Army %s will be zombie",
+        value_help = "<LOC zombies_0003>Slow %s will be zombie",
         values = {
-            '1','2','3','4','5','6','7','8','9','10','11','12',
+            '1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'
+        }
+    },
+    {
+        default = 1,
+        label = "<LOC zombies_0001>Zombies: Vampire",
+        help = "<LOC zombies_0002>Recieve Mass and Energy cost of whatever you kill",
+        key = 'VampirePercentage',
+        value_text = "%s",
+        value_help = "<LOC zombies_0003>%s times mass & energy vampire",
+        values = {
+            '0','0.25','0.5','0.75','1.0','1.25','1.5','1.75','2.0'
         }
     }
 }
