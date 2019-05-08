bef83a72994de3c73fcdac068583abba4083a8f0
Douglas Gaskell <1400380+douglasg14b@users.noreply.github.com>
2019-05-07 23:22:16 -0700

    Adds Build Speed Buff definitions

---
+++ hook/lua/sim/BuffDefinitions.lua
@@ -0,0 +1,99 @@
+do
+    BuffBlueprint {
+        Name = "ZombieBuildRate_1.25",
+        DisplayName = "ZombieBuildRate_1.25",
+        BuffType = "ZOMBIE_BuildRate",
+        Stacks = 'ALWAYS',
+        Duration = -1,
+        Affects = {
+            BuildRate = {
+                Add = 0,
+                Mult = 1.25
+            },
+        },
+    }
+
+    BuffBlueprint {
+        Name = "ZombieBuildRate_1.5",
+        DisplayName = "ZombieBuildRate_1.5",
+        BuffType = "ZOMBIE_BuildRate",
+        Stacks = 'ALWAYS',
+        Duration = -1,
+        Affects = {
+            BuildRate = {
+                Add = 0,
+                Mult = 1.5
+            },
+        },
+    }
+
+    BuffBlueprint {
+        Name = "ZombieBuildRate_1.75",
+        DisplayName = "ZombieBuildRate_1.75",
+        BuffType = "ZOMBIE_BuildRate",
+        Stacks = 'ALWAYS',
+        Duration = -1,
+        Affects = {
+            BuildRate = {
+                Add = 0,
+                Mult = 1.75
+            },
+        },
+    }
+
+    BuffBlueprint {
+        Name = "ZombieBuildRate_2",
+        DisplayName = "ZombieBuildRate_2",
+        BuffType = "ZOMBIE_BuildRate",
+        Stacks = 'ALWAYS',
+        Duration = -1,
+        Affects = {
+            BuildRate = {
+                Add = 0,
+                Mult = 2.0
+            },
+        },
+    }
+
+    BuffBlueprint {
+        Name = "ZombieBuildRate_2.5",
+        DisplayName = "ZombieBuildRate_2.5",
+        BuffType = "ZOMBIE_BuildRate",
+        Stacks = 'ALWAYS',
+        Duration = -1,
+        Affects = {
+            BuildRate = {
+                Add = 0,
+                Mult = 2.5
+            },
+        },
+    }
+
+    BuffBlueprint {
+        Name = "ZombieBuildRate_3",
+        DisplayName = "ZombieBuildRate_3",
+        BuffType = "ZOMBIE_BuildRate",
+        Stacks = 'ALWAYS',
+        Duration = -1,
+        Affects = {
+            BuildRate = {
+                Add = 0,
+                Mult = 3.0
+            },
+        },
+    }
+
+    BuffBlueprint {
+        Name = "ZombieBuildRate_4",
+        DisplayName = "ZombieBuildRate_4",
+        BuffType = "ZOMBIE_BuildRate",
+        Stacks = 'ALWAYS',
+        Duration = -1,
+        Affects = {
+            BuildRate = {
+                Add = 0,
+                Mult = 4.0
+            },
+        },
+    }
+end
