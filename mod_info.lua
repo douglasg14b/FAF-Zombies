a9e1083227ad41e2b3b6be418525a416736bd8b8
Douglas Gaskell <1400380+douglasg14b@users.noreply.github.com>
2019-07-09 12:49:50 -0700

    Updates mod info and version

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "Zombies"
-uid = "bec6916c-4e74-45e2-86fe-359a3d023324"
-version = 6
+uid = "90292fb8-cab5-4f48-9e76-d9066119bc89"
+version = 7
 description = "Dead units will become zombies! Now with army selection, mass/energy vampire, zombies speed, and zombie decay under AI Options in the lobby. See more at https://github.com/douglasg14b/FAF-Zombies"
 author = "Douglasg14b. Original Code by Zarechaun. Icon by flaticon.com"
 url = "https://github.com/douglasg14b/FAF-Zombies"
